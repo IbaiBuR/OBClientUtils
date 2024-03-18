bc870936f30640235a58df98edd24579fe6b17a9
IbaiBuR <ibaibur@chessnibble.com>
2024-03-19 00:16:31 +0100

    Fix comments of batch script

--- openbench.bat
+++ openbench.bat
@@ -6,16 +6,16 @@ set USERNAME="%3"
 set PASSWORD="%4"
 set REPO="%5"
 
-# Install dependencies via chocolatey
+REM Install dependencies via chocolatey
 choco install make winlibs rust golang -y
 
-# Refresh environtment variables
+REM Refresh environtment variables
 @RefreshEnv.cmd
 
-# Clone client and install dependencies
+REM Clone client and install dependencies
 git clone "%REPO"
 cd OpenBench\Client
 python -m pip install -r requirements.txt
 
-# Run worker
+REM Run worker
 python .\client.py -U "%USERNAME" -P "%PASSWORD" -S "%SERVER" --clean -N 1 -T "%THREADS"
